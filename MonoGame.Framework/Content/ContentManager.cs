zignd/MonoGame

commit 71f8bfb5e2b67885cf0b7c0fb7149a531b7573cf

Content.Load<Texture>() fallback to load from file when no .xnb exists (#8582)
The PR is a reply to the feature request
https://github.com/MonoGame/MonoGame/issues/8481.

For the longest time, I had a class in my game project that looks like
the PoC code posted by [squarebananas](https://github.com/squarebananas)
in the discussion so I decided to tackle this issue.

### Description of Change

- Adds the capability to load Texture2D from PNG, JPG/JPEG and BMP files
without using the content pipeline.
- Fixes the documentation of TitleContainer.OpenStream which was
incorrect.
- Adds placeholder comment for related feature requests
https://github.com/MonoGame/MonoGame/issues/8482
https://github.com/MonoGame/MonoGame/issues/8483
https://github.com/MonoGame/MonoGame/issues/8484.
- Adds an alternate internal method to ContainerTitle.FromStream that
doesn't rely on exceptions for signalling errors.

### What it doesn't do

The fallback discussed in issue 8481 proposed a "smart" way to search
for a file when originally not found. For instance, locating it in
different subfolders, or using different casing.

I think that "advanced" behavior is distinct from the ability to load a
file without the content pipeline. It is also involving more code
changes, for a benefit I cannot see, as some rigor is expected anyway
from the developer. Adding more magic to the system would likely make it
harder to use/debug in the end.

### Testing

Manually tested against my game in DesktopGL on macOS M1.

MonoGame.Framework/Content/ContentManager.cs
@@ -70,6 +70,7 @@ namespace Microsoft.Xna.Framework.Content
             'l', // Linux
         };
 
+        private static readonly string[] supportedTexture2DExtensions = new string[] { ".png", ".jpg", ".jpeg", ".bmp" };
 
         static partial void PlatformStaticInit();
 
@@ -148,7 +149,7 @@ namespace Microsoft.Xna.Framework.Content
         /// </summary>
         /// <remarks>
         ///     <para>
-        ///         By default, the ContentMangaer searches for content in the directory where the executable is located.
+        ///         By default, the ContentManager searches for content in the directory where the executable is located.
         ///     </para>
         ///     <para>
         ///         When creating a new ContentManager, if no instance of <see cref="Game"/> is otherwise required by
@@ -297,6 +298,7 @@ namespace Microsoft.Xna.Framework.Content
         /// Before a ContentManager can load an asset, you need to add the asset to your game project using
         /// the steps described in
         /// <see href="https://docs.monogame.net/articles/getting_started/content_pipeline/index.html">Adding Content - MonoGame</see>.
+        /// <br>PNG, JPG/JPEG and BMP files can be loaded as Texture2D without using the content pipeline. The assetName must not contain extension.
         /// </remarks>
         /// <typeparam name="T">
         ///     <para>
@@ -345,7 +347,7 @@ namespace Microsoft.Xna.Framework.Content
             {
                 throw new ObjectDisposedException("ContentManager");
             }
-            if(Path.IsPathRooted(assetName))
+            if (Path.IsPathRooted(assetName))
             {
                 throw new ContentLoadException("assetName '" + assetName + "' cannot be a rooted (absolute) path. Remove any leading drive letters (e.g. 'C:'), forward slashes or backslashes");
             }
@@ -372,8 +374,8 @@ namespace Microsoft.Xna.Framework.Content
 
             // Load the asset.
             result = ReadAsset<T>(assetName, null);
-
             loadedAssets[key] = result;
+
             return result;
 		}
 
@@ -383,7 +385,7 @@ namespace Microsoft.Xna.Framework.Content
 			Stream stream;
 			try
             {
-                var assetPath = Path.Combine(RootDirectory, assetName) + ".xnb";
+                string assetPath = Path.Combine(RootDirectory, assetName) + ".xnb";
 
                 // This is primarily for editor support.
                 // Setting the RootDirectory to an absolute path is useful in editor
@@ -408,7 +410,6 @@ namespace Microsoft.Xna.Framework.Content
 			{
 				throw new ContentLoadException("The content file was not found.", fileNotFound);
 			}
-
 			catch (DirectoryNotFoundException directoryNotFound)
 			{
 				throw new ContentLoadException("The directory was not found.", directoryNotFound);
@@ -417,6 +418,7 @@ namespace Microsoft.Xna.Framework.Content
 			{
 				throw new ContentLoadException("Opening stream error.", exception);
 			}
+            
 			return stream;
 		}
 
@@ -435,20 +437,52 @@ namespace Microsoft.Xna.Framework.Content
 			string originalAssetName = assetName;
 			object result = null;
 
-            // Try to load as XNB file
-            var stream = OpenStream(assetName);
-            using (var xnbReader = new BinaryReader(stream))
+            try
             {
-                using (var reader = GetContentReaderFromXnb(assetName, stream, xnbReader, recordDisposableObject))
+                // Try to load as XNB file
+                var stream = OpenStream(assetName);
+                using (var xnbReader = new BinaryReader(stream))
                 {
-                    result = reader.ReadAsset<T>();
-                    if (result is GraphicsResource)
-                        ((GraphicsResource)result).Name = originalAssetName;
+                    using (var reader = GetContentReaderFromXnb(assetName, stream, xnbReader, recordDisposableObject))
+                    {
+                        result = reader.ReadAsset<T>();
+                        if (result is GraphicsResource)
+                        {
+                            ((GraphicsResource)result).Name = originalAssetName;
+                        }
+                    }
+                }
+            }
+            catch (ContentLoadException)
+            {
+                try
+                {
+                    // If the file is not found, we try searching a file with differents extensions
+                    // based on the type of asset searched (e.g. '.bmp' and '.png' for a Texture2D)
+                    switch (typeof(T).Name)
+                    {
+                        case nameof(Texture2D):
+                            result = LoadTexture2DFromImageFile(assetName);
+                            break;
+                        // Futures cases here (see relevant GitHub Issues):
+                        // Video
+                        // Song
+                        // SoundEffect
+                        // Texture3D (?)
+                        default:
+                            break;
+                    }
+                }
+                catch
+                {
+                    result = null;
                 }
             }
 
 			if (result == null)
+            {
 				throw new ContentLoadException("Could not load " + originalAssetName + " asset!");
+            }
 
 			return (T)result;
 		}
@@ -505,6 +539,27 @@ namespace Microsoft.Xna.Framework.Content
                                                         originalAssetName, version, recordDisposableObject);
 
             return reader;
+        }
+    
+        Texture2D LoadTexture2DFromImageFile(string assetName)
+        {
+            IGraphicsDeviceService graphicsDeviceService = serviceProvider.GetService(typeof(IGraphicsDeviceService)) as IGraphicsDeviceService;
+            
+            foreach (string extension in supportedTexture2DExtensions)
+            {
+                string assetPath = Path.Combine(RootDirectory, assetName);
+                assetPath = Path.ChangeExtension(assetPath, extension);
+
+                using Stream file = TitleContainer.OpenStreamNoException(assetPath);
+                if (file != null)
+                {
+                    Texture2D result = Texture2D.FromStream(graphicsDeviceService.GraphicsDevice, file, DefaultColorProcessors.PremultiplyAlpha);
+
+                    return result;
+                }
+            }
+
+            return null;
         }
 
         internal void RecordDisposable(IDisposable disposable)
@@ -575,7 +630,9 @@ namespace Microsoft.Xna.Framework.Content
 		    foreach (var disposable in disposableAssets)
 		    {
 		        if (disposable != null)
+                {
 		            disposable.Dispose();
+                }
 		    }
 			disposableAssets.Clear();
 		    loadedAssets.Clear();

.github/workflows/main.yml
@@ -219,8 +219,17 @@ jobs:
         env:
           CI: true
 
-      - name: Run DirectX Tests
-        run: dotnet MonoGame.Tests.dll
+      # Run the DirectX tests in two steps: first the audio, then the rest.
+      # This is because the audio tests has some incompatibilities within the runner with ContentManagerTests.
+      - name: Run DirectX Audio Tests
+        run: dotnet test --filter Category=Audio MonoGame.Tests.dll 
+        env:
+          CI: true
+        working-directory: tests-windowsdx
+        if: runner.os == 'Windows'
+        
+      - name: Run DirectX All Tests Except Audio
+        run: dotnet test --filter Category!=Audio MonoGame.Tests.dll 
         env:
           CI: true
         working-directory: tests-windowsdx

MonoGame.Framework/TitleContainer.cs
@@ -27,7 +27,9 @@ namespace Microsoft.Xna.Framework
         /// Returns an open stream to an existing file in the title storage area.
         /// </summary>
         /// <param name="name">The filepath relative to the title storage area.</param>
-        /// <returns>A open stream or null if the file is not found.</returns>
+        /// <returns>An open stream if file is found.</returns>
+        /// <exception cref="ArgumentNullException">If name is null or invalid.</exception>
+        /// <exception cref="FileNotFoundException">If file is not found.</exception>
         public static Stream OpenStream(string name)
         {
             if (string.IsNullOrEmpty(name))
@@ -59,6 +61,30 @@ namespace Microsoft.Xna.Framework
             }
 
             return stream;
+        }
+
+        /// <summary>
+        /// Faster version of <see cref="OpenStream"/> as it doesn't rely on exceptions in error cases.
+        /// </summary>
+        internal static Stream OpenStreamNoException(string name)
+        {
+            if (string.IsNullOrEmpty(name) || Path.IsPathRooted(name))
+            {
+                return null;
+            }
+
+            string safeName = NormalizeRelativePath(name);
+            Stream stream;
+            try
+            {
+                stream = PlatformOpenStream(safeName);
+                
+                return stream;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
 
         private static Exception FileNotFoundException(string name, Exception inner)

Tests/Framework/Content/ContentManagerTest.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Tests.Graphics;
 using NUnit.Framework;
 
 namespace MonoGame.Tests.Content
 {
-    public class ContentManagerTest
+    [TestFixture]
+    [NonParallelizable]
+    internal class ContentManagerTest : GraphicsDeviceTestFixtureBase
     {
         [TestCase("C:\\image.png")]
         [TestCase("\\image.png")]
@@ -16,6 +20,52 @@ namespace MonoGame.Tests.Content
             ContentManager content = new ContentManager(services, "Content");
             var exception = Assert.Throws<ContentLoadException>(() => content.Load<Texture2D>(assetName));
             StringAssert.Contains("rooted (absolute)", exception.Message);
+        }
+
+        [Test]
+        // Tests loading a texture from a XNB file
+        public void CorrectlyLoadTextureFromXnb()
+        {
+            ContentManager content = new ContentManager(game.Services);
+            
+            Texture2D texture = content.Load<Texture2D>(Paths.Texture("MonoGameIcon"));
+
+            Assert.IsNotNull(texture);
+        }
+
+        [Test]
+        [TestCase("UniquePng")]
+        [TestCase("UniqueBmp")]
+        [TestCase("UniqueJpg")]
+        [TestCase("UniqueJpeg")]
+        // Tests loading from a PNG/JPG/JPEG/BMP file when a corresponding XNB file doesn't exist
+        public void CorrectlyLoadTextureFromAlternativeImageFormatsWhenNoXnb(string assetName)
+        {
+            ContentManager content = new ContentManager(game.Services);
+
+            Texture2D texture = content.Load<Texture2D>(Paths.Texture(assetName));
+
+            Assert.IsNotNull(texture);
+        }
+
+        [Test]
+        // Tests that an exception is raised when no XNB, PNG, JPG, JPEG or BMP exists for a content name
+        public void ThrowExceptionIfNoAssetInAnySupportedImageFormats()
+        {
+            ContentManager content = new ContentManager(game.Services);
+
+            var exception = Assert.Throws<ContentLoadException>(() => content.Load<Texture2D>(Paths.Texture("NotExisting")));
+            StringAssert.StartsWith("Could not load", exception.Message);
+        }
+
+        [Test]
+        // Tests that an exception is raised when a non XNB format exists but the content type requested is not a Texture
+        public void ThrowExceptionIfTypeIsNotTexture()
+        {
+            ContentManager content = new ContentManager(game.Services);
+
+            var exception = Assert.Throws<ContentLoadException>(() => content.Load<SoundEffect>(Paths.Texture("UniquePng")));
+            StringAssert.StartsWith("Could not load", exception.Message);
         }
     }
 }

commit 91b9e1d8d2c703bb0e12281931867d010e73d43f

[Documentation] Minor XML documentation fixes (#8675)
### Description of Change
This PR:
a) fixes the typo docline in `ContentManager.cs` introduced in
https://github.com/MonoGame/MonoGame/commit/71f8bfb5e2b67885cf0b7c0fb7149a531b7573cf#diff-37705868f9e9f6e1ee0f3b9f8d532d01f861d3e26a3050b30435ac545abb81ceR301

b) adds **empty** doclines for readers that were made public in
https://github.com/MonoGame/MonoGame/commit/396bf96ca183a01161faa78618cce2d35c4b31f8
(this fixes all
[CS1591](https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/compiler-messages/cs1591)
warnings in DesktopGL and partly in WindowsDX):
`Content/ContentReaders/ArrayReader.cs`
`Content/ContentReaders/DictionaryReader.cs`
`Content/ContentReaders/EnumReader.cs`
`Content/ContentReaders/ListReader.cs`
`Content/ContentReaders/MultiArrayReader.cs`
`Content/ContentReaders/NullableReader.cs`
`Content/ContentReaders/ReflectiveReader.cs`
Feel free to revert this change, if actual documentation is to be added
to these files.

MonoGame.Framework/Content/ContentManager.cs
@@ -297,8 +297,8 @@ namespace Microsoft.Xna.Framework.Content
         /// <remarks>
         /// Before a ContentManager can load an asset, you need to add the asset to your game project using
         /// the steps described in
-        /// <see href="https://docs.monogame.net/articles/getting_started/content_pipeline/index.html">Adding Content - MonoGame</see>.
-        /// <br>PNG, JPG/JPEG and BMP files can be loaded as Texture2D without using the content pipeline. The assetName must not contain extension.
+        /// <see href="https://docs.monogame.net/articles/getting_started/content_pipeline/index.html">Adding Content - MonoGame</see>. <br/>
+        /// PNG, JPG/JPEG and BMP files can be loaded as Texture2D without using the content pipeline. The assetName must not contain extension.
         /// </remarks>
         /// <typeparam name="T">
         ///     <para>

MonoGame.Framework/Content/ContentReaders/ArrayReader.cs
@@ -17,16 +17,19 @@ namespace Microsoft.Xna.Framework.Content
     {
         ContentTypeReader elementReader;
 
+        /// <summary/>
         public ArrayReader()
         {
         }
 
+        /// <summary/>
         protected internal override void Initialize(ContentTypeReaderManager manager)
 		{
 			Type readerType = typeof(T);
 			elementReader = manager.GetTypeReader(readerType);
         }
 
+        /// <summary/>
         protected internal override T[] Read(ContentReader input, T[] existingInstance)
         {
             uint count = input.ReadUInt32();

MonoGame.Framework/Content/ContentReaders/DictionaryReader.cs
@@ -21,11 +21,13 @@ namespace Microsoft.Xna.Framework.Content
 		
 		Type keyType;
 		Type valueType;
-		
+
+        /// <summary/>
         public DictionaryReader()
         {
         }
 
+        /// <summary/>
         protected internal override void Initialize(ContentTypeReaderManager manager)
         {
 			keyType = typeof(TKey);
@@ -35,11 +37,13 @@ namespace Microsoft.Xna.Framework.Content
 			valueReader = manager.GetTypeReader(valueType);
         }
 
+        /// <summary/>
         public override bool CanDeserializeIntoExistingObject
         {
             get { return true; }
         }
 
+        /// <summary/>
         protected internal override Dictionary<TKey, TValue> Read(ContentReader input, Dictionary<TKey, TValue> existingInstance)
         {
             int count = input.ReadInt32();

MonoGame.Framework/Content/ContentReaders/EnumReader.cs
@@ -16,16 +16,19 @@ namespace Microsoft.Xna.Framework.Content
     {
         ContentTypeReader elementReader;
 
+        /// <summary/>
         public EnumReader()
         {
         }
 
+        /// <summary/>
         protected internal override void Initialize(ContentTypeReaderManager manager)
         {			
 			Type readerType = Enum.GetUnderlyingType(typeof(T));
 			elementReader = manager.GetTypeReader(readerType);
         }
-		
+
+        /// <summary/>
         protected internal override T Read(ContentReader input, T existingInstance)
         {
 			return input.ReadRawObject<T>(elementReader);

MonoGame.Framework/Content/ContentReaders/ListReader.cs
@@ -18,21 +18,25 @@ namespace Microsoft.Xna.Framework.Content
     {
         ContentTypeReader elementReader;
 
+        /// <summary/>
         public ListReader()
         {
         }
 
+        /// <summary/>
         protected internal override void Initialize(ContentTypeReaderManager manager)
         {
 			Type readerType = typeof(T);
 			elementReader = manager.GetTypeReader(readerType);
         }
 
+        /// <summary/>
         public override bool CanDeserializeIntoExistingObject
         {
             get { return true; }
         }
 
+        /// <summary/>
         protected internal override List<T> Read(ContentReader input, List<T> existingInstance)
         {
             int count = input.ReadInt32();

MonoGame.Framework/Content/ContentReaders/MultiArrayReader.cs
@@ -17,14 +17,17 @@ namespace Microsoft.Xna.Framework.Content
     {
         ContentTypeReader elementReader;
 
+        /// <summary/>
         public MultiArrayReader() { }
 
+        /// <summary/>
         protected internal override void Initialize(ContentTypeReaderManager manager)
         {
             Type readerType = typeof(T);
             elementReader = manager.GetTypeReader(readerType);
         }
 
+        /// <summary/>
         protected internal override Array Read(ContentReader input, Array existingInstance)
         {
             var rank = input.ReadInt32();

MonoGame.Framework/Content/ContentReaders/NullableReader.cs
@@ -16,16 +16,19 @@ namespace Microsoft.Xna.Framework.Content
     {
         ContentTypeReader elementReader;
 
+        /// <summary/>
         public NullableReader()
         {
         }
 
+        /// <summary/>
         protected internal override void Initialize(ContentTypeReaderManager manager)
         {			
 			Type readerType = typeof(T);
 			elementReader = manager.GetTypeReader(readerType);
         }
-		
+
+        /// <summary/>
         protected internal override T? Read(ContentReader input, T? existingInstance)
         {
 			if(input.ReadBoolean())

MonoGame.Framework/Content/ContentReaders/ReflectiveReader.cs
@@ -27,17 +27,19 @@ namespace Microsoft.Xna.Framework.Content
 
         private ContentTypeReader _baseTypeReader;
 
-
+        /// <summary/>
         public ReflectiveReader() 
             : base(typeof(T))
         {
         }
 
+        /// <summary/>
         public override bool CanDeserializeIntoExistingObject
         {
             get { return TargetType.IsClass(); }
         }
 
+        /// <summary/>
         protected internal override void Initialize(ContentTypeReaderManager manager)
         {
             base.Initialize(manager);
@@ -170,7 +172,8 @@ namespace Microsoft.Xna.Framework.Content
                 setter(parent, obj2);
             };
         }
-      
+
+        /// <summary/>
         protected internal override object Read(ContentReader input, object existingInstance)
         {
             T obj;