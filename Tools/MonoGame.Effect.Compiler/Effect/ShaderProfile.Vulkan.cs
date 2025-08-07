zignd/MonoGame

commit 51c1395cc879848baee912a75c19cdb5de912ea6

Fixes SPIR-V globals key lookup & parsing int/uint/float input types (#8544)

Tools/MonoGame.Effect.Compiler/Effect/ShaderProfile.Vulkan.cs
@@ -380,7 +380,7 @@ namespace MonoGame.Effect
 
                 // First gather the uniforms.
                 VkStruct globals;
-                if (structs.TryGetValue("%type__MG_Globals", out globals))
+                if (structs.TryGetValue("%type__Globals", out globals))
                 {
                     foreach (var member in globals.members.Values)
                         cbuffer.AddParameter(member.name, member.type, 0, member.offset);
@@ -449,24 +449,29 @@ namespace MonoGame.Effect
                         }
 
                         int size;
-                        if (input.type.StartsWith("v"))
+                        int len;
+                        int inputTypeStringStartIndex;
+                        if (input.type.StartsWith("v") && char.IsDigit(input.type[1]))
                         {
-                            int len = (int)char.GetNumericValue(input.type[1]);
-                            switch (input.type.Substring(2))
-                            {
-                                case "int":
-                                case "float":
-                                    size = len * 4;
-                                    break;
-                                default:
-                                    errorsAndWarnings += string.Format("Unknown vertex shader input type '{0}'.", input.type);
-                                    throw new ShaderCompilerException();
-                            }
+                            len = (int)char.GetNumericValue(input.type[1]);
+                            inputTypeStringStartIndex = 2;
                         }
                         else
                         {
-                            errorsAndWarnings += string.Format("Unknown vertex shader input type '{0}'.", input.type);
-                            throw new ShaderCompilerException();
+                            len = 1;
+                            inputTypeStringStartIndex = 0;
+                        }
+
+                        switch (input.type.Substring(inputTypeStringStartIndex))
+                        {
+                            case "int":
+                            case "uint":
+                            case "float":
+                                size = len * 4;
+                                break;
+                            default:
+                                errorsAndWarnings += string.Format("Unknown vertex shader input type '{0}'.", input.type);
+                                throw new ShaderCompilerException();
                         }
 
                         offset += size;

Tools/MonoGame.Effect.Compiler/Effect/ConstantBufferData.Vulkan.cs
@@ -15,6 +15,7 @@ namespace MonoGame.Effect
             {
                 case "float":
                     return EffectObject.D3DXPARAMETER_TYPE.FLOAT;
+                case "uint":
                 case "int":
                     return EffectObject.D3DXPARAMETER_TYPE.INT;
                 case "bool":

commit 16b218d95789582425b2172f05f21bfd731fa158

Fix Vulkan constant buffer reflection data parsing (#8941)
This fixes a regression introduced by #8544 in Vulkan constant buffers
parsing.

This should repair shader building and usage (notably the stock
shaders).

Tools/MonoGame.Effect.Compiler/Effect/ShaderProfile.Vulkan.cs
@@ -394,12 +394,30 @@ namespace MonoGame.Effect
 
                 // TODO: Support multiple constant buffers one day!
 
-                // First gather the uniforms.
+                int cbCount = 0;
                 VkStruct globals;
-                if (structs.TryGetValue("%type__Globals", out globals))
+                foreach (var descriptor in descriptors)
                 {
-                    foreach (var member in globals.members.Values)
-                        cbuffer.AddParameter(member.name, member.type, 0, member.offset);
+                    // Find uniform buffers.
+                    if (descriptor.Value.type == VkDescriptorType.UNIFORM_BUFFER)
+                    {
+                        // Check if there is a corresponding struct.
+                        if (names.TryGetValue(descriptor.Key, out string name))
+                        {
+                            if (structs.TryGetValue("%type_" + name, out globals))
+                            {
+                                if (++cbCount > 1)
+                                {
+                                    errorsAndWarnings += "Building effects for Vulkan currently doesn't support more than one constant buffer (cbuffer) structures. Please consider refactoring your HLSL code.";
+                                    throw new ShaderCompilerException();
+                                }
+
+                                // Gather uniforms.
+                                foreach (var member in globals.members.Values)
+                                    cbuffer.AddParameter(member.name, member.type, 0, member.offset);
+                            }
+                        }
+                    }
                 }
 
                 // Gather the input attributes.