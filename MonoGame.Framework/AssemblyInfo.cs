500ca0bc6dd8d3008f3ad9ed6d2885c563e8f25b
zignd <hello@zignd.dev>
2026-07-31 14:11:14 -0300

    feat(ui): add component catalog

--- MonoGame.Framework/AssemblyInfo.cs
+++ MonoGame.Framework/AssemblyInfo.cs
@@ -5,4 +5,5 @@ using System.Runtime.CompilerServices;
 [assembly:InternalsVisibleTo("MonoGame.Effect")]
 [assembly:InternalsVisibleTo("MonoGame.Tools.Tests")]
 [assembly:InternalsVisibleTo("MonoGame.ShaderPipeline.Benchmark")]
+[assembly:InternalsVisibleTo("MonoGame.UI.Catalog")]
 
---
+++ Tools/MonoGame.UI.Catalog/Program.cs
@@ -0,0 +1,4 @@
+using MonoGame.UI.Catalog;
+
+using var game = new CatalogGame(CatalogMetricsOptions.Parse(args));
+game.Run();
